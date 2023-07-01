3c37015052ac40639646c92d2926bb56865fcda7
Joan Rotbard <joanrotbard@gmail.com>
2023-07-01 16:57:13 -0300

    open link in new tab from social

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -9,6 +9,7 @@ const Socials: FC = memo(() => {
         <a
           aria-label={label}
           className="-m-1.5 rounded-md p-1.5 transition-all duration-300 hover:text-yellow-500 focus:outline-none focus:ring-2 focus:ring-yellow-500  sm:-m-3 sm:p-3"
+          target='_blank'
           href={href}
           key={label}>
           <Icon className="h-5 w-5 align-baseline sm:h-6 sm:w-6" />
