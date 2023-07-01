7df7a128d2a628e306766611027e048a163aca1b
Joan Rotbard <joanrotbard@gmail.com>
2023-07-01 16:59:35 -0300

    lint

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -9,9 +9,9 @@ const Socials: FC = memo(() => {
         <a
           aria-label={label}
           className="-m-1.5 rounded-md p-1.5 transition-all duration-300 hover:text-yellow-500 focus:outline-none focus:ring-2 focus:ring-yellow-500  sm:-m-3 sm:p-3"
-          target='_blank'
           href={href}
-          key={label}>
+          key={label}
+          target="_blank">
           <Icon className="h-5 w-5 align-baseline sm:h-6 sm:w-6" />
         </a>
       ))}
